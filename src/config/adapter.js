75baac1d50be4aa3e88aa2bb45212ac6252949fd
lulifeng <lulifeng_vngc@bupt.edu.cn>
2018-05-01 18:23:35 +0800

    20180501 1

--- src/config/adapter.js
+++ src/config/adapter.js
@@ -36,15 +36,15 @@ exports.model = {
   },
   mysql: {
     handle: mysql,
-    database: 'nideshop',
-    //database: 'onlinemyshop',
+    //database: 'nideshop',
+    database: 'onlinemyshop',
     prefix: 'nideshop_',
     //prefix: 'myshop_',
     encoding: 'utf8mb4',
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: 'root',
+    password: 'LLFeng100...',
     dateStrings: true
   }
 };
